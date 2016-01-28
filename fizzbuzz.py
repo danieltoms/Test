9babf2a51002c57c9179dda9eb816a369e369322
danieltoms <daniel_toms@hotmail.com>
2016-01-28 14:12:11 +0000

    This is my first commit

---
+++ fizzbuzz.py
@@ -0,0 +1,12 @@
+HowFar = int(input("How far to count"))
+while HowFar < 1:
+    HowFar = int(input("Not a valid number, please try again"))
+for myloop in range (1, HowFar +1):
+    if myloop %3 == 0 and myloop %5 == 0:
+        print("FizzBuzz")
+    elif myloop % 3 == 0:
+        print("Fizz")
+    elif myloop % 5 == 0:
+        print("Buzz")
+    else:
+        print(myloop)
